86521fb8422585e2a5451a1627d899c5b3a1edcc
mvidal <mvidal@gmail.com>
2010-09-03 17:52:26 -0300

    controllers for the pediatrician form

---
+++ presentation/src/main/java/com/tda/presentation/controller/EditPediatricianFormController.java
@@ -0,0 +1,55 @@
+package com.tda.presentation.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import com.tda.model.pediatrician.PediatricianForm;
+import com.tda.service.api.PediatricianFormService;
+
+@Controller
+@RequestMapping("/patient/*/pediatrician/{pediatricianFormId}/edit")
+@SessionAttributes("pediatricianForm")
+public class EditPediatricianFormController extends
+		BasePediatricianFormController {
+	private static final String PEDIATRICIAN_FORM = "pediatricianform/form";
+	private PediatricianFormService pediatricianFormService;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String setupForm(
+			@PathVariable("pediatricianFormId") long pediatricianFormId,
+			Model model) {
+		PediatricianForm pediatricianForm = pediatricianFormService
+				.findById(pediatricianFormId);
+		model.addAttribute("pediatricianForm", pediatricianForm);
+		return PEDIATRICIAN_FORM;
+	}
+
+	@RequestMapping(method = { RequestMethod.POST })
+	public String processSubmit(
+			@Valid @ModelAttribute("pediatricianForm") PediatricianForm pediatricianForm,
+			BindingResult result, SessionStatus status) {
+		if (result.hasErrors()) {
+			return PEDIATRICIAN_FORM;
+		} else {
+			pediatricianFormService.update(pediatricianForm);
+			status.setComplete();
+			return "redirect:/patient/" + pediatricianForm.getPatient().getId();
+		}
+	}
+
+	@Autowired
+	public void setPediatricianFormService(
+			PediatricianFormService pediatricianFormService) {
+		this.pediatricianFormService = pediatricianFormService;
+	}
+}
