78033d0cd7ea4e589d5154f6abd8238e1bae8e4e
iandrono <ivan.andronowicz@gmail.com>
2011-04-16 13:57:38 -0300

    Cambios en la fachada de sync

--- presentation/src/main/java/com/tda/presentation/controller/SyncController.java
+++ presentation/src/main/java/com/tda/presentation/controller/SyncController.java
@@ -27,7 +27,6 @@ public class SyncController {
 	@RequestMapping(value = "/do", method = RequestMethod.GET)
 	public ModelAndView sync(@Valid @ModelAttribute ConfigSync configSync,
 			BindingResult result) {
-		Process process;
 		String respMessage = "";
 		// Variables a parametrizas
 		String maatkitDir = "/Users/iandrono/Documents/ITBA/PF/maatkit-7332";
@@ -42,31 +41,44 @@ public class SyncController {
 		} else {
 
 			try {
-				process = Runtime.getRuntime().exec(
-						"perl " + maatkitDir
-								+ "/bin/mk-table-sync --execute h=localhost:"
-								+ localPort + ",u=" + bothDBUser + ",p="
-								+ bothDBpass + " --databases tda h="
-								+ configSync.getIp() + ":"
-								+ configSync.getPort() + " --verbose");
-				process.waitFor();
-				/*
-				 * STATUS MEANING ======
-				 * ======================================================= 0
-				 * Success. 1 Internal error. 2 At least one table differed on
-				 * the destination. 3 Combination of 1 and 2.
-				 */
-				switch (process.exitValue()) {
-				case 0:
-					respMessage = "No hubia cambios pendientes";
-					break;
-				case 1:
-					respMessage = "Sincronizacion abortada por error interno";
-				case 2:
-					respMessage = "Cambios sincronizados!";
-					break;
-				case 3:
-					respMessage = "Hubo cambios pero termino con error";
+
+				Runtime runtime = Runtime.getRuntime();
+				Process process = runtime.exec("perl " + maatkitDir
+						+ "/bin/mk-table-sync --execute h=localhost:"
+						+ localPort + ",u=" + bothDBUser + ",p=" + bothDBpass
+						+ " --databases tda h=" + configSync.getIp() + ":"
+						+ configSync.getPort() + " --verbose --wait 0");
+
+				Worker worker = new Worker(process);
+				worker.start();
+				try {
+					worker.join(10000);
+					if (worker.exit != null)
+						/*
+						 * STATUS MEANING ======
+						 * ================================
+						 * ======================= 0 Success. 1 Internal error.
+						 * 2 At least one table differed on the destination. 3
+						 * Combination of 1 and 2.
+						 */
+						switch (worker.exit) {
+						case 0:
+							respMessage = "No hubia cambios pendientes";
+							break;
+						case 1:
+							respMessage = "Sincronizacion abortada por error interno";
+						case 2:
+							respMessage = "Cambios sincronizados!";
+							break;
+						case 3:
+							respMessage = "Hubo cambios pero termino con error";
+						}
+					else
+						respMessage = "Timeout: intente mas tarde";
+				} catch (InterruptedException ex) {
+					worker.interrupt();
+				} finally {
+					process.destroy();
 				}
 			} catch (Exception e) {
 				System.out.println("Exception: " + e.toString());
@@ -74,5 +86,22 @@ public class SyncController {
 			model.addObject("resultMessage", respMessage);
 		}
 		return model;
+	}
+}
+
+class Worker extends Thread {
+	private final Process process;
+	Integer exit;
+
+	Worker(Process process) {
+		this.process = process;
+	}
+
+	public void run() {
+		try {
+			exit = process.waitFor();
+		} catch (InterruptedException ignore) {
+			return;
+		}
 	}
 }
