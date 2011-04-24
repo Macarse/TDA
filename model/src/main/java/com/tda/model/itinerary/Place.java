a371869f89ef60c428ed30fb41015a7ef340a0f8
mvidal <mvidal@gmail.com>
2011-04-24 17:51:40 -0300

    Added model for the itinerary.

---
+++ model/src/main/java/com/tda/model/itinerary/Place.java
@@ -0,0 +1,70 @@
+package com.tda.model.itinerary;
+
+import java.util.Date;
+
+public class Place {
+	private Date arrivalDate;
+	private Date departureDate;
+	private String country;
+	private String province;
+	private String city;
+	private String neighbourhood;
+	private String additionalInfo;
+
+	public Date getArrivalDate() {
+		return arrivalDate;
+	}
+
+	public void setArrivalDate(Date arrivalDate) {
+		this.arrivalDate = arrivalDate;
+	}
+
+	public Date getDepartureDate() {
+		return departureDate;
+	}
+
+	public void setDepartureDate(Date departureDate) {
+		this.departureDate = departureDate;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getNeighbourhood() {
+		return neighbourhood;
+	}
+
+	public void setNeighbourhood(String neighbourhood) {
+		this.neighbourhood = neighbourhood;
+	}
+
+	public String getAdditionalInfo() {
+		return additionalInfo;
+	}
+
+	public void setAdditionalInfo(String additionalInfo) {
+		this.additionalInfo = additionalInfo;
+	}
+
+}
