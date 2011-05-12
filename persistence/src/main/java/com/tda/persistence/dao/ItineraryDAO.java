58d70c6bb7c02723296db95bb7af0aa3b32f6918
mvidal <mvidal@gmail.com>
2011-05-12 18:14:17 -0300

    Method to find the next itinerary.

--- persistence/src/main/java/com/tda/persistence/dao/ItineraryDAO.java
+++ persistence/src/main/java/com/tda/persistence/dao/ItineraryDAO.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 
 import com.tda.model.itinerary.Itinerary;
@@ -22,4 +23,22 @@ public class ItineraryDAO extends GenericDAOImpl<Itinerary> {
 		return getHibernateTemplate().findByCriteria(crit);
 	}
 
+	public Itinerary findNextItinerary() {
+		Date currentDate = new Date();
+		List<Itinerary> currentItineraries = findItineraryForDate(currentDate);
+		if (currentItineraries.size() > 0) {
+			return currentItineraries.get(0);
+		}
+
+		DetachedCriteria crit = DetachedCriteria.forClass(Itinerary.class);
+		crit.add(Restrictions.ge("beginningDate", currentDate));
+		crit.addOrder(Order.asc("beginningDate"));
+		List<Itinerary> nextItineraries = getHibernateTemplate()
+				.findByCriteria(crit);
+		if (nextItineraries.size() > 0) {
+			return nextItineraries.get(0);
+		}
+
+		return null;
+	}
 }
