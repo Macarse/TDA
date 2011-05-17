fcad85c774a64daddcf3be832b48f9422c616bb0
Maximiliano Gomez Vidal <mvidal@gmail.com>
2011-05-17 15:38:13 -0300

    Eager fetch for the places.

--- model/src/main/java/com/tda/model/itinerary/Itinerary.java
+++ model/src/main/java/com/tda/model/itinerary/Itinerary.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -76,7 +77,7 @@ public class Itinerary {
 		this.description = description;
 	}
 
-	@ManyToMany(cascade = CascadeType.ALL)
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	public List<Place> getPlaces() {
 		return places;
 	}
