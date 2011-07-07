4239cf151e84055a3f6f573c8739f03b721aac0d
iandrono <ivan.andronowicz@gmail.com>
2011-07-06 22:37:06 -0300

    Agregado el provider de PediatricianDiagnosis para levantar automaticamente los diagnosticos

--- model/src/main/java/com/tda/model/pediatrician/PediatricianDiagnosis.java
+++ model/src/main/java/com/tda/model/pediatrician/PediatricianDiagnosis.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 
 import javax.persistence.Basic;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -18,7 +16,6 @@ public class PediatricianDiagnosis implements
 	private String description;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
