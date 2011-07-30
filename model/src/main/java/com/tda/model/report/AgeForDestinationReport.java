963aeaa0fc1293d3d98a19c416e65ae6e9782b0e
iandrono <ivan.andronowicz@gmail.com>
2011-07-30 01:47:52 -0300

    reporte de edades por destinos

---
+++ model/src/main/java/com/tda/model/report/AgeForDestinationReport.java
@@ -0,0 +1,33 @@
+package com.tda.model.report;
+
+public class AgeForDestinationReport {
+
+	private String destination;
+	private String age;
+	private Integer quantity;
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+}
