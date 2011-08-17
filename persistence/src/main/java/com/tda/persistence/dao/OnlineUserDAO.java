bdaf446f3bfb4f0582a7ed6ab7d80b7eb4e829bf
pabloks <pabloks@zeus.RED>
2011-08-16 23:18:31 -0300

    optimize query

--- persistence/src/main/java/com/tda/persistence/dao/OnlineUserDAO.java
+++ persistence/src/main/java/com/tda/persistence/dao/OnlineUserDAO.java
@@ -1,8 +1,13 @@
 package com.tda.persistence.dao;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.springframework.orm.hibernate3.HibernateCallback;
 
 import com.tda.model.applicationuser.OnlineUser;
 
@@ -15,11 +20,21 @@ public class OnlineUserDAO extends GenericDAOImpl<OnlineUser> {
 	
 	@SuppressWarnings("unchecked")
 	public void deleteTimeoutUsers(){
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String now = sdf.format(new Date());
-		List<OnlineUser> list = getHibernateTemplate().find("from OnlineUser ou WHERE timeout <= '" + now + "'");
-		for (OnlineUser onlineUser : list) {
-			this.delete(onlineUser);
-		}
+		getHibernateTemplate().execute(new HibernateCallback(){
+
+			@SuppressWarnings("deprecation")
+			public Object doInHibernate(Session session)
+					throws HibernateException, SQLException {
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				String now = sdf.format(new Date());
+				
+				String query = "DELETE FROM OnlineUser ou WHERE timeout <= '" + now + "'";
+				
+				Statement st = session.connection().createStatement();
+				st.executeQuery(query);
+				
+				return null;
+			}
+		});
 	}
 }
